ac47d24c575136ced8920efe8f437cb7e14f610e
mzishankhan <mzishankhan@tripadvisor.com>
2022-10-28 01:27:17 +0530

    updated port

--- src/main/java/com/example/springtest/TestController.java
+++ src/main/java/com/example/springtest/TestController.java
@@ -9,6 +9,6 @@ public class TestController {
     @GetMapping("/hello")
     public String getHello() {
         System.out.println("Request received!!");
-        return "Hello Docker new msg";
+        return "Hello Docker new msg1";
     }
 }
